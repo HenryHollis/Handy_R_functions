f616ab117d768ab0aa5a2f1802dc1c8222e94bac
Henry Hollis <41795183+HenryHollis@users.noreply.github.com>
2022-07-26 12:50:13 -0400

    Update handy_r_functions.R

--- handy_r_functions.R
+++ handy_r_functions.R
@@ -1,9 +1,10 @@
-average_duplicates(mixed_df, key_col, skip_rows = NULL){
+average_duplicates = function(mixed_df,skip_rows = NULL){
+  mixed_df = as.data.frame(mixed_df)
   if(!is.null(skip_rows)){
     untouched_rows = unname(mixed_df[skip_rows,])
     mixed_df = mixed_df[-skip_rows, ]
   }
-  mixed_df[, -1] = sapply(mixed_df[, -1], as.numeric)
+  mixed_df[, -1] = as.data.frame(sapply(mixed_df[, -1], as.numeric))
   mixed_df = aggregate(mixed_df[,-1], list(Gene=mixed_df[,1]), FUN = mean)
   if(!is.null(skip_rows)){
     mixed_df = rbind(untouched_rows, mixed_df)
